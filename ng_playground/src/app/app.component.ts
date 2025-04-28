14fc1fa9444b19a77ebf1df7f5671d0f266884a9
MARVIN <o.silverygold@gmail.com>
2025-04-28 22:14:43 +0300

    init app

--- ng_playground/src/app/app.component.ts
+++ ng_playground/src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = 'ng_playground';
+  title = 'PLAYGROUND';
 }
