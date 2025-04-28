c32bc46ed31b4e8517bdaaddc9b5f2a502511d5a
MARVIN <o.silverygold@gmail.com>
2025-04-28 22:09:32 +0300

    add greeting component with input

---
+++ ng_playground/src/app/components/greeting/greeting.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-greeting',
+  imports: [],
+  templateUrl: './greeting.component.html',
+  styleUrl: './greeting.component.css'
+})
+export class GreetingComponent {
+  @Input() message = "Hello, hello!"
+}
