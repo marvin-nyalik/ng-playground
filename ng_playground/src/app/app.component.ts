002ffa0aa1af7ee76db3098ccc338d53b3396557
MARVIN <o.silverygold@gmail.com>
2025-04-29 21:44:09 +0300

    Add useful comments

--- ng_playground/src/app/app.component.ts
+++ ng_playground/src/app/app.component.ts
@@ -3,8 +3,10 @@ import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './components/header/header.component';
 
+// An Angular component is a building block of the UI.
+
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', //how you embed a component in another template, custom html tag
   imports: [RouterOutlet, HomeComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
