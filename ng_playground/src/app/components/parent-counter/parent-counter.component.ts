117b633fd6a1aadb7a8a7af4849fa449fb6cb681
MARVIN <o.silverygold@gmail.com>
2025-05-01 19:38:28 +0300

    Alter user to track changes with reference update

--- ng_playground/src/app/components/parent-counter/parent-counter.component.ts
+++ ng_playground/src/app/components/parent-counter/parent-counter.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { ChildCounterComponent } from '../child-counter/child-counter.component';
+import { UserComponent } from '../user/user.component';
+import { CheckerComponent } from "../checker/checker.component";
 
 @Component({
   selector: 'app-parent-counter',
-  imports: [ChildCounterComponent],
+  imports: [ChildCounterComponent, UserComponent, CheckerComponent],
   templateUrl: './parent-counter.component.html',
   styleUrl: './parent-counter.component.css'
 })
 export class ParentCounterComponent {
   count = 0;
+  user = {id: 7248289, firstName: 'Melisa', lastName:"Akinyi", birthday: new Date(14/7/2007)};
 
   increment = () => {
     this.count++;
@@ -16,5 +19,9 @@ export class ParentCounterComponent {
 
   decrement = () => {
     this.count--;
+  }
+
+  updateUser(){
+    this.user = {...this.user, lastName: "Marvelline"}
   }
 }
